6300ea6a584a8abd403de07bf6fe14439929e3ce
Ryan Carlson <americanized@gmail.com>
2021-04-09 09:22:48 -0700

    Getting a random number 0-2

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("My name is Ryan!");
+let ranNum;
+
+function computerPlay() {
+    ranNum = Math.floor(Math.random() * 3);
+    return ranNum;
+}
+
+computerPlay();
+
+if (ranNum == 0) {
+    answer = 'Rock';
+}
+else if (ranNum == 1) {
+    answer = 'Paper';
+}
+else if (ranNum == 2) {
+    answer = 'Scissors'
+}
+
+console.log(ranNum);
+console.log(answer);
