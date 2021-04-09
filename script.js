04a0856e5f335a3957a9d77f37f90731403bd415
Ryan Carlson <76183152+Henry-Goodboy@users.noreply.github.com>
2021-04-09 12:24:53 -0700

    player and computer can choose an option, option displayed in console

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
+
+//Declare variable outside of function
+let caseInput;
+let playerSelection;
+
+//Allow the player to pick an option, option needs to be case neutral.
+function userPlay() {
+    anycaseInput = prompt('Rock, Paper or Scissors?');
+
+    playerSelection = anycaseInput.toUpperCase();
+
+    if (playerSelection == 'ROCK') {
+        console.log('Rock');
+    }
+    else if (playerSelection == 'PAPER') {
+        console.log('Paper');
+    }
+    else if (playerSelection == 'SCISSORS') {
+        console.log('Scissors');
+    }
+    else {
+        console.log('Invalid Input');
+    }
+}
+
+//Run userPlay function
+userPlay();
+
+//Declare variable outside of function
 let ranNum;
 
+//Computer picks option at random
 function computerPlay() {
+
     ranNum = Math.floor(Math.random() * 3);
-    return ranNum;
-}
 
+    if (ranNum == 0) {
+        computerSelection = 'Rock';
+    }
+    else if (ranNum == 1) {
+        computerSelection = 'Paper';
+    }
+    else if (ranNum == 2) {
+        computerSelection = 'Scissors';
+    }
+    
+    console.log(computerSelection);
+   
+}
+//Run computerPlay function
 computerPlay();
-
-if (ranNum == 0) {
-    answer = 'Rock';
-}
-else if (ranNum == 1) {
-    answer = 'Paper';
-}
-else if (ranNum == 2) {
-    answer = 'Scissors'
-}
-
-console.log(answer);
