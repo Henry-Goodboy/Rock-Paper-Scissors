ad544c8e9a17110baa88575fb08b2459dddb8de1
Ryan Carlson <76183152+Henry-Goodboy@users.noreply.github.com>
2021-04-09 20:12:42 -0700

    Prettier :)

--- script.js
+++ script.js
@@ -1,112 +1,100 @@
-let computerScore = 0;
-let playerScore = 0;
+let computerScore = 0
+let playerScore = 0
 let champ
 
-function game() {
+function game () {
+  //Player picks option, option is formatted properly
+  function playerSelection () {
+    rawUserInput = prompt('Rock, Paper or Scissors?')
+    userInput =
+      rawUserInput.charAt(0).toUpperCase() + rawUserInput.slice(1).toLowerCase()
+    return userInput
+  }
 
-//Player picks option, option is formatted properly
-function playerSelection() {
-    rawUserInput = prompt('Rock, Paper or Scissors?');
-    userInput = rawUserInput.charAt(0).toUpperCase() + rawUserInput.slice(1).toLowerCase();
-    return userInput;
-}
-
-//Computer picks option at random
-function computerPlay() {
-
-    ranNum = Math.floor(Math.random() * 3);
+  //Computer picks option at random
+  function computerPlay () {
+    ranNum = Math.floor(Math.random() * 3)
 
     if (ranNum == 0) {
-        computerSelection = 'Rock';
+      computerSelection = 'Rock'
+    } else if (ranNum == 1) {
+      computerSelection = 'Paper'
+    } else if (ranNum == 2) {
+      computerSelection = 'Scissors'
     }
-    else if (ranNum == 1) {
-        computerSelection = 'Paper';
-    }
-    else if (ranNum == 2) {
-        computerSelection = 'Scissors';
-    }
-    return computerSelection;
-}
+    return computerSelection
+  }
 
-//making it easier to write by subbing variables
-let w = 'Play again.'
-let x = 'Paper covers Rock.'
-let y = 'Rock smashes Scissors.'
-let z = 'Scissors cut Paper.'
-let result
+  //making it easier to write by subbing variables
+  let w = 'Play again.'
+  let x = 'Paper covers Rock.'
+  let y = 'Rock smashes Scissors.'
+  let z = 'Scissors cut Paper.'
+  let result
 
-//this is where the game decisions are made
-function playRound(a,b) {
+  //this is where the game decisions are made
+  function playRound (a, b) {
     if (a == b) {
-        winner = 'No one'
-        result = w
-    }    
-    else if (a == 'Paper' && b == 'Rock') {
-        winner = ('Player')
-        result = x
-    }    
-    else if (a == 'Scissors' && b == 'Rock') {
-        winner = ('Computer')
-        result = y
-    }    
-    else if (a == 'Scissors' && b == 'Paper') {
-        winner = ('Player')
-        result = z
-    }    
-    else if (a == 'Rock' && b == 'Paper') {
-        winner = ('Computer')
-        result = x
-    }    
-    else if (a == 'Rock' && b == 'Scissors') {
-        winner = ('Player')
-        result = y
-    }    
-    else if (a == 'Paper' && b == 'Scissors') {
-        winner = ('Computer')
-        result = z
+      winner = 'No one'
+      result = w
+    } else if (a == 'Paper' && b == 'Rock') {
+      winner = 'Player'
+      result = x
+    } else if (a == 'Scissors' && b == 'Rock') {
+      winner = 'Computer'
+      result = y
+    } else if (a == 'Scissors' && b == 'Paper') {
+      winner = 'Player'
+      result = z
+    } else if (a == 'Rock' && b == 'Paper') {
+      winner = 'Computer'
+      result = x
+    } else if (a == 'Rock' && b == 'Scissors') {
+      winner = 'Player'
+      result = y
+    } else if (a == 'Paper' && b == 'Scissors') {
+      winner = 'Computer'
+      result = z
+    } else {
+      winner = 'Invalid input. No one'
+      result = w
     }
-    else {
-        winner = 'Invalid input. No one'
-        result = w
-    }     
-}
+  }
 
-playRound( playerSelection(), computerPlay() );
+  playRound(playerSelection(), computerPlay())
 
-//end of game score keeper
-function endScore() {
+  //end of game score keeper
+  function endScore () {
     if (winner == 'Player') {
-        playerScore += 1;
+      playerScore += 1
+    } else if (winner == 'Computer') {
+      computerScore += 1
     }
-    else if (winner == 'Computer') {
-        computerScore += 1;
-    }
-}
+  }
 
-endScore();
+  endScore()
 
-console.log(`You picked: ${ userInput } and the Computer picked: ${ computerSelection }`);
-console.log(`${winner} wins.`);
-console.log(result);
-
+  console.log(
+    `You picked: ${userInput} and the Computer picked: ${computerSelection}`
+  )
+  console.log(`${winner} wins.`)
+  console.log(result)
 }
 
 //run it five times
-game();
-game();
-game();
-game();
-game();
+game()
+game()
+game()
+game()
+game()
 
 //log relevant information in the console
-console.log(`Player ${playerScore}`);
-console.log(`Computer ${computerScore}`);
+console.log(`Player ${playerScore}`)
+console.log(`Computer ${computerScore}`)
 if (playerScore > computerScore) {
-    console.log('Player Wins!')
-}
-else if (playerScore < computerScore) {
-    console.log('Computer Wins!')
-}
-else {
-    console.log('Tied Game - Play Again')
+  console.log('Player Wins!')
+} else if (playerScore < computerScore) {
+  console.log('Computer Wins!')
+} else {
+  console.log('Tied Game - Play Again')
 }
